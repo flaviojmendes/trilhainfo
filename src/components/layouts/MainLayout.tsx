e91e1de082b0c89c0d3de9a9344a6438e803299c
Flavio Mendes <flaviojmendes@gmail.com>
2022-06-26 11:02:30 +0100

    adding footer

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -7,22 +7,36 @@ type Props = {
 };
 
 export default function MainLayout({ children }: Props) {
-  return (<>
-    <div className="container p-2 flex mx-auto mt-2">
-        <ChakraLink
-          isExternal
-          className="mr-0 ml-auto flex"
-          textDecoration={"none"}
-          href="https://github.com/flaviojmendes/trilhadev"
-        >
-          <FaGithubSquare className="w-8 h-8 c-red" />
-          <span className="my-auto text-lg ml-1 c-red">Github</span>
-        </ChakraLink>
-      </div>
-      <div className="container py-1 px-2 mx-auto mt-0 mb-10">
-        <Logo />
+  return (
+    <>
+      <div className="flex flex-col h-screen">
+        <div className="container p-2 flex mx-auto mt-2">
+          <ChakraLink
+            isExternal
+            className="mr-0 ml-auto flex"
+            textDecoration={"none"}
+            href="https://github.com/flaviojmendes/trilhadev"
+          >
+            <FaGithubSquare className="w-8 h-8 c-red" />
+            <span className="my-auto text-lg ml-1 c-red">Github</span>
+          </ChakraLink>
+        </div>
+        <div className="container flex-grow py-1 px-2 mx-auto mt-0 mb-10">
+          <Logo />
 
-        {children}
+          {children}
+        </div>
+        <footer className="text-center pb-4">
+          <span className="c-brown">Idealizado por </span>
+          <ChakraLink
+            isExternal
+            color={"#ee8561"}
+            href="https://github.com/flaviojmendes"
+          >
+            flaviojmendes
+          </ChakraLink>
+          <span className="c-brown"> e mantido pela comunidade.</span>
+        </footer>
       </div>
     </>
   );
