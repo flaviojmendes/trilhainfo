752524f75b2bae4fa052569e93c2dee65280c639
Flavio Mendes <flaviojmendes@gmail.com>
2022-06-27 13:46:45 +0100

    adding checkboxes for content you already studied

--- src/components/Roadmap/Roadmap.tsx
+++ src/components/Roadmap/Roadmap.tsx
@@ -8,6 +8,8 @@ import {
   Badge,
   Box,
   Button,
+  Checkbox,
+  CheckboxGroup,
   Flex,
   Link,
   Modal,
@@ -21,19 +23,48 @@ import {
   useDisclosure,
 } from "@chakra-ui/react";
 
-import React from "react";
+import React, { useEffect } from "react";
+import { useLocalStorage } from "react-use";
 import { Level, RoadmapItem } from "../../entity/RoadmapItem";
 import LevelItem from "../Level/LevelItem";
 
-
 type Props = {
-    data: Level[];
-    title: string;
-}
+  data: Level[];
+  title: string;
+};
 
 export default function Roadmap(props: Props) {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [activeItem, setActiveItem] = React.useState<RoadmapItem>();
+  const [selectedItems, setSelectedItems, remove] = useLocalStorage(
+    "selectedItems",
+    {} as { [key: string]: boolean }
+  );
+
+  useEffect(() => {
+    if (localStorage.getItem("selectedItems")) {
+      setSelectedItems(
+        JSON.parse(localStorage.getItem("selectedItems") || "") || {}
+      );
+    }
+  }, []);
+
+  function saveRead(label: string, checked: boolean) {
+    let selected = selectedItems;
+    if(!selected) {
+      selected = {};
+    }
+    selected[label] = checked;
+    setSelectedItems(selected);
+    localStorage.setItem("selectedItems", JSON.stringify(selected));
+  }
+
+  function isRead(label: string) {
+    if (selectedItems) {
+      return selectedItems[label];
+    }
+    return false;
+  }
 
   return (
     <>
@@ -67,10 +98,24 @@ export default function Roadmap(props: Props) {
               {activeItem?.children?.map((child, index) => {
                 return (
                   <AccordionItem key={child.label}>
-                    
                     <h2 className="font-semibold">
                       <AccordionButton>
                         <Box flex="1" textAlign="left">
+                          <CheckboxGroup>
+                            <Checkbox
+                              className="my-auto mr-2"
+                              size={"lg"}
+                              isChecked={isRead(
+                                child.label + "-" + activeItem.label
+                              )}
+                              onChange={(e) => {
+                                saveRead(
+                                  child.label + "-" + activeItem.label,
+                                  e.target.checked
+                                );
+                              }}
+                            ></Checkbox>
+                          </CheckboxGroup>
                           {child.label}
                         </Box>
                         <AccordionIcon />
