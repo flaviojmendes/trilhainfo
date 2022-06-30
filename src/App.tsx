861e7de5310c5ae9ad7f8ee96e723e9fe5709860
Flavio Mendes <flaviojmendes@gmail.com>
2022-06-30 08:45:18 +0100

    analytics

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
 import RoadmapPage from "./pages/RoadmapPage";
-import ReactGA from "react-ga";
+import ReactGA from "react-ga4";
 
 function App() {
   
   ReactGA.initialize( 'G-GQDL3TBPEZ');
-  ReactGA.pageview(window.location.pathname + window.location.search);
+  ReactGA.send("pageview", window.location.pathname + window.location.search);
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
