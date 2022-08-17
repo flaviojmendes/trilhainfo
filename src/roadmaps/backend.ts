c45f45b550c45a969cb029dbc97c5550cb36cadd
Jéssica <jessicanathany.web@gmail.com>
2022-08-17 19:48:08 -0300

    modified:   src/roadmaps/backend.ts

--- src/roadmaps/backend.ts
+++ src/roadmaps/backend.ts
@@ -1031,11 +1031,24 @@ export const data: Level[] = [
       {
         label: "Mitigação de Erros",
         children: [
-          { label: "Graceful Degradation", links: [] },
+          { label: "Graceful Degradation", 
+            links:[
+              {
+                label: "Graceful Degradation - Georgia Tech - Advanced Operating Systems", 
+                url: "https://www.youtube.com/watch?v=Tk7e0LMsAlI", 
+                contentType: LinkContentType.WATCH
+              },
+            ] },
           { label: "Throttling", links: [] },
           { label: "Backpressure", links: [] },
           { label: "Loadshifting", links: [] },
-          { label: "Circuit Breaker", links: [] },
+          { label: "Circuit Breaker", 
+            links: [
+              {
+                label:"Circuit Breaker: Não trabalhe com microsserviços sem saber disso", 
+                url: "https://www.youtube.com/watch?v=hxRn8y5KTzQ",
+                contentType: LinkContentType.WATCH,
+              },] },
         ],
       },
       {
@@ -1050,6 +1063,65 @@ export const data: Level[] = [
       {
         label: "Observabilidade",
         children: [{ label: "O básico", links: [] }],
+      },
+    ],
+  },
+  {
+    items: [
+      {
+        label: "Cloud Computing",
+        description:
+          "Cloud computing, conhecida também como computação em nuvem, é a tecnologia que permite o uso remoto de recursos da computação por meio da conectividade da Internet.",
+        children: [
+          {
+            label: "AWS",
+            links: [
+              {
+                url: "https://www.youtube.com/watch?v=j6yImUbs4OA&list=PLOF5f9_x-OYUaqJar6EKRAonJNSHDFZUm",
+                label: "O que é AWS e como aprender mais com o Curso Gratuito Amazon Web Services na Prática - Aula 01 - #32",
+                type: LinkType.FREE,
+                votes: 0,
+                contentType: LinkContentType.WATCH,
+              },
+            ],
+          },
+          {
+            label: "Azure",
+            links: [
+              {
+                url: "https://www.youtube.com/watch?v=AITM8sTsu-o&list=PLwftZeDnOzt0quETXYfGjfi2AcpeGX-7i",
+                label: "Azure Fundamentals - Introdução ao Curso - Ray Carneiro",
+                type: LinkType.FREE,
+                votes: 0,
+                contentType: LinkContentType.WATCH,
+              },
+            ],
+          },
+          {
+            label: "Google Platfom",
+            links: [
+              {
+                url: "https://www.youtube.com/watch?v=4lZmhCaq13U",
+                label: "Google Cloud, por onde começar e por quê você deveria faze-lo",
+                type: LinkType.FREE,
+                votes: 0,
+                contentType: LinkContentType.WATCH,
+              },
+            ],
+          },
+          {
+            label: "Digital Ocean",
+            links: [
+              {
+                url: "https://www.youtube.com/watch?v=eu6DCSv95MQ&list=PLVKPmJRbmf2Rb2KwvGzRP1eBhQCWrRbhE",
+                label: "Curso Digital Ocean - Introdução ao Curso - Aula 01",
+                type: LinkType.FREE,
+                votes: 0,
+                contentType: LinkContentType.WATCH,
+              },
+            ],
+          },
+        ],
       },
     ],
   },
