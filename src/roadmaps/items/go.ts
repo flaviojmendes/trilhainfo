f3739705e7ce339c8eeaef9269487512eeed0cf8
Rodrigo Adão dos Santos <rodrigoadaodossantos@gmail.com>
2022-12-14 15:37:41 -0300

    fix: #131 Conteudo de GoLang não abre

--- src/roadmaps/items/go.ts
+++ src/roadmaps/items/go.ts
@@ -6,26 +6,33 @@ import {
 
 export const go: RoadmapItem = {
   label: "Go",
-  links: [
+  description:
+    "Golang é uma linguagem de programação criada pela Google e lançada em código livre em novembro de 2019",
+  children: [
     {
-      label: "Aprenda Go",
-      url: "https://www.youtube.com/watch?v=WiGU_ZB-u0w&list=PLCKpcjBB_VlBsxJ9IseNxFllf-UFEXOdg",
-      contentType: LinkContentType.WATCH,
-    },
-    {
-      label: "Go 101 - Tiago Temporin",
-      url: "https://tiago-temporin.notion.site/Go-101-1fe9cef0ccc94ed3bed7f38e7dd5815d",
-      contentType: LinkContentType.VISIT,
-    },
-    {
-      label: "Aprenda Go - Ellen Korbes",
-      url: "https://www.youtube.com/c/AprendaGo/about",
-      contentType: LinkContentType.VISIT,
-    },
-    {
-      label: "Aprenda Go com Testes - Lauren Ferreira ",
-      url: "https://larien.gitbook.io/aprenda-go-com-testes/",
-      contentType: LinkContentType.VISIT,
-    },
+      label: "Go",
+      links: [
+        {
+          label: "Aprenda Go",
+          url: "https://www.youtube.com/watch?v=WiGU_ZB-u0w&list=PLCKpcjBB_VlBsxJ9IseNxFllf-UFEXOdg",
+          contentType: LinkContentType.WATCH,
+        },
+        {
+          label: "Go 101 - Tiago Temporin",
+          url: "https://tiago-temporin.notion.site/Go-101-1fe9cef0ccc94ed3bed7f38e7dd5815d",
+          contentType: LinkContentType.VISIT,
+        },
+        {
+          label: "Aprenda Go - Ellen Korbes",
+          url: "https://www.youtube.com/c/AprendaGo/about",
+          contentType: LinkContentType.VISIT,
+        },
+        {
+          label: "Aprenda Go com Testes - Lauren Ferreira ",
+          url: "https://larien.gitbook.io/aprenda-go-com-testes/",
+          contentType: LinkContentType.VISIT,
+        },
+      ],
+    }
   ],
 };
