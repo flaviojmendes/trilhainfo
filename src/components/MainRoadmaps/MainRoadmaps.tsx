acf4ef530021d8d9228901bdb97ea834e944296f
Flavio Mendes <flaviojmendes@gmail.com>
2023-01-14 12:28:12 +0000

    testing

--- src/components/MainRoadmaps/MainRoadmaps.tsx
+++ src/components/MainRoadmaps/MainRoadmaps.tsx
@@ -1,92 +1,45 @@
+import { FaArrowRight } from "react-icons/fa";
+import { BsArrowRightCircleFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import { mainRoadmaps } from "./mainRoadmaps";
 
 export default function MainRoadmaps() {
   return (
-    <div id="mainRoadmaps" className="pt-4">
-      <h2 className="text-center txt-title my-6  text-3xl c-yellow">
-        Tudo o que você precisa saber no universo de:
+    <div id="mainRoadmaps" className="pt-4 xl:px-64 px-10">
+      <h2 className="txt-title font-semibolds md:w-1/3 my-6 text-4xl c-yellow">
+        Tudo o que você{" "}
+        <span className="text-red txt-title">precisa saber</span> no universo
+        de:
       </h2>
 
-      <section className="flex flex-wrap items-stretch space-y-10 pb-10 md:space-y-0 justify-center px-2 gap-5">
-        {/* Frontend */}
-        <Link
-          className="bg-brown md:w-1/3 lg:w-1/4 w-full min-h-fit hover:bg-white py-3 rounded-md"
-          to={"/roadmap/frontend"}
-        >
-          <img
-            className="w-1/2 mx-auto"
-            src="/frontend.png"
-          />
-          <h3 className="text-center text-3xl txt-title mb-2 c-dark-brown">
-            Frontend
-          </h3>
-          <p className="m-6">
-            A pessoa que desenvolve front-end é responsável pela experiência do
-            usuário dentro de uma aplicação web. É ela quem vai desenhar e
-            desenvolver as páginas com as quais, posteriormente, o usuário irá
-            interagir.
-          </p>
-        </Link>
-
-        {/* Backend */}
-        <Link
-          className="bg-brown md:w-1/3 lg:w-1/4 w-full min-h-fit hover:bg-white py-3 rounded-md"
-          to={"/roadmap/backend"}
-        >
-          <img
-            className="w-1/2 mx-auto"
-            src="/backend.png"
-          />
-          <h3 className="text-center text-3xl txt-title mb-2 c-dark-brown">
-            Backend
-          </h3>
-          <p className="m-6">
-            Back-end se relaciona com o que está por trás das aplicações
-            desenvolvidas na programação. Ou seja, tudo que dá estrutura e apoio
-            às ações do usuário da máquina é chamado de back-end.
-          </p>
-        </Link>
-
-        {/* Devops */}
-        <Link
-          className="bg-brown md:w-1/3 lg:w-1/4 w-full min-h-fit hover:bg-white py-3 rounded-md"
-          to={"/roadmap/devops"}
-        >
-          {" "}
-          <img
-            className="w-1/2 mx-auto"
-            src="/devops.png"
-          />
-          <h3 className="text-center text-3xl txt-title mb-2 c-dark-brown">
-            Devops
-          </h3>
-          <p className="m-6">
-            Especialista em DevOps (Desenvolvimento e Operações, de maneira
-            simplificada) é a pessoa que atua na integração entre as equipes de
-            desenvolvimento de software, especialmente nas áreas de
-            desenvolvimento e operações, integrando e monitorando suas
-            atividades para buscar um desempenho mais otimizado e simplificado.
-          </p>
-        </Link>
-
-        {/* React */}
-        <Link
-          className="bg-brown md:w-1/3 lg:w-1/4 w-full min-h-fit hover:bg-white py-3 rounded-md"
-          to={"/roadmap/react"}
-        >
-          <img
-            className="w-1/2 mx-auto p-4"
-            src="/react.svg"
-          />
-          <h3 className="text-center text-3xl txt-title mb-2 c-dark-brown">
-            React
-          </h3>
-          <p className="m-6">
-            React é uma biblioteca JavaScript declarativa, eficiente e flexível
-            criada pelo Facebook em 2011 para desenvolver aplicações front-end,
-            ou seja, interfaces de usuário (UIs).
-          </p>
-        </Link>
+      <section className="flex flex-wrap items-stretch space-y-10 pb-10 md:space-y-0 gap-5 w-full mx-auto">
+        {mainRoadmaps.map((mainRoadmap) => {
+          return (
+            <Link
+              className="bg-brown md:w-1/3 lg:w-1/4 w-full hover:bg-white py-3 pr-2 rounded-md flex"
+              to={mainRoadmap.url}
+            >
+              <div className="w-2/3 flex flex-col">
+                <h3 className="ml-4 font-semibold text-3xl txt-title mb-2 c-dark-brown">
+                  {mainRoadmap.title}
+                </h3>
+                <p className="m-4 text-sm txt-title">
+                  {mainRoadmap.description}
+                </p>
+                <div className="grow"></div>
+                <div className="flex ml-4 text-red">
+                  <span className="txt-title underline my-auto font-semibold">
+                    Comece agora 
+                  </span>
+                  <BsArrowRightCircleFill  className="my-auto ml-2"/>
+                </div>
+              </div>
+              <div className="w-1/3 flex">
+                <img className="w-full m-auto" src={mainRoadmap.image} />
+              </div>
+            </Link>
+          );
+        })}
       </section>
     </div>
   );
