d3805ce6d44cbeb3a30425affcb427d49cd57a99
Nicholas Costa <nicholascostadev@gmail.com>
2023-01-17 14:50:42 -0300

    remove some unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
--- src/components/ProjectInfo/ProjectInfo.tsx
+++ src/components/ProjectInfo/ProjectInfo.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { cheatSheets } from "../../guides/cheatSheets";
 import { GoPlus } from "react-icons/go";
 
