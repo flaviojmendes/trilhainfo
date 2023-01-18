37fe79ecd08f9a0bb02d656872a6afe2237edaad
Juliano Sirtori <julianosirtori@gmail.com>
2023-01-18 19:40:14 -0300

    :lipstick: add rules to eslint

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,10 @@
-const prettierPluginTailwindcss = require("prettier-plugin-tailwindcss");
+const prettierPluginTailwindcss = require('prettier-plugin-tailwindcss');
 
 module.exports = {
   plugins: [prettierPluginTailwindcss],
+  printWidth: 100,
+  tabWidth: 2,
+  arrowParens: 'always',
+  singleQuote: true,
+  trailingComma: 'all',
 };
