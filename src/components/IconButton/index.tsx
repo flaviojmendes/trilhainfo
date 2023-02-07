acf283dd1c7e94f6a44968cc4c772ff0a1c9ddb1
Nicholas Costa <nicholascostadev@gmail.com>
2023-02-07 14:56:22 -0300

    feat: forward IconButton's ref

--- src/components/IconButton/index.tsx
+++ src/components/IconButton/index.tsx
@@ -1,17 +1,23 @@
+import { forwardRef } from 'react';
 interface IconButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   icon: React.ReactNode;
-};
+}
 
-export default function IconButton(props: IconButtonProps) {
-  const {"aria-label": ariaLabel, icon, className, ...rest} = {...props}
+const IconButton = forwardRef<HTMLButtonElement, IconButtonProps>((props, ref) => {
+  const { 'aria-label': ariaLabel, icon, className, ...rest } = { ...props };
 
   return (
     <button
-     {...rest}
-     className={`inline-flex justify-center items-center h-10 min-w-[2.5rem] bg-[#EDF2F7] hover:bg-[#E2E8F0] disabled:hover:bg-[#E2E8F0]/40 disabled:bg-[#E2E8F0]/40 disabled:cursor-not-allowed transition-colors rounded-md ${className}`}
+      {...rest}
+      className={`inline-flex h-10 min-w-[2.5rem] items-center justify-center rounded-md bg-[#EDF2F7] transition-colors hover:bg-[#E2E8F0] disabled:cursor-not-allowed disabled:bg-[#E2E8F0]/40 disabled:hover:bg-[#E2E8F0]/40 ${className}`}
+      ref={ref}
     >
       {ariaLabel && <span className="sr-only">{ariaLabel}</span>}
       {icon}
     </button>
-  )
-}
+  );
+});
+
+IconButton.displayName = 'IconButton';
+
+export default IconButton;
