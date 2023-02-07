592b4e6875dd7e31c82bf2f8b8cd0f053e322bad
Nicholas Costa <nicholascostadev@gmail.com>
2023-02-07 00:31:50 -0300

    refactor: remove chakra

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,22 @@
 import { Auth0Provider } from '@auth0/auth0-react';
-import { ChakraProvider } from '@chakra-ui/react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import ToastProvider from './contexts/ToastContext';
 import './index.css';
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <ChakraProvider>
-    <ToastProvider>
-      <BrowserRouter>
-        <Auth0Provider
-          domain="trilha-info.us.auth0.com"
-          clientId="ZPGRWe1CKiY84dCDtfTm6yy3ibSQbH4s"
-          redirectUri={window.location.origin + '/login'}
-          audience="TrilhaInfoApi"
-        >
-          <App />
-        </Auth0Provider>
-      </BrowserRouter>
-    </ToastProvider>
-  </ChakraProvider>
+  <ToastProvider>
+    <BrowserRouter>
+      <Auth0Provider
+        domain="trilha-info.us.auth0.com"
+        clientId="ZPGRWe1CKiY84dCDtfTm6yy3ibSQbH4s"
+        redirectUri={window.location.origin + '/login'}
+        audience="TrilhaInfoApi"
+      >
+        <App />
+      </Auth0Provider>
+    </BrowserRouter>
+  </ToastProvider>,
 );
