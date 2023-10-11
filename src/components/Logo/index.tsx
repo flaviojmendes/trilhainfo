3d59b6d3780035ef9fc28c229aa98fa4d37328d1
Flavio Mendes <flavio.mendes@workday.com>
2023-10-11 13:32:01 +0100

    updating few things

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 export default function Logo() {
   return (
     <NavLink to="/">
-      <img className="w-64 lg:w-80" src="/logo.svg" alt="logo" />
+      <img className="w-48 lg:w-64" src="/logo.svg" alt="logo" />
     </NavLink>
   );
 }
