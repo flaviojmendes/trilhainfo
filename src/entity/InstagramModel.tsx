976c12fdd30a4a4cc8cc14cc2f711b310671f41d
Flavio Mendes <flavio.mendes@workday.com>
2024-05-17 14:43:40 +0100

    glossary

---
+++ src/entity/InstagramModel.tsx
@@ -0,0 +1,8 @@
+export interface InstagramMedia {
+  id: string;
+  caption: string;
+  media_type: 'IMAGE' | 'VIDEO' | 'CAROUSEL_ALBUM';
+  media_url: string;
+  permalink: string;
+  thumbnail_url?: string;
+}
--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { Grid } from 'react-loader-spinner';
 import Cookies from 'universal-cookie';
-import { User } from '../entity/UserModel';
+import { User, mapResponseToUser, UserApiResponse } from '../entity/UserModel';
 import MainLayout from '../components/layouts/MainLayout';
 import { decode } from 'base-64';
 
@@ -26,13 +26,16 @@ export default function UserPage() {
         const email = JSON.parse(payload.toString())['https://trilha.info/email'];
 
         try {
-          const response = await axios.get(import.meta.env.VITE_API_URL + '/users/' + email, {
-            headers: {
-              'Content-Type': 'application/json',
-              Authorization: cookies.get('api_token'),
+          const response = await axios.get<UserApiResponse>(
+            import.meta.env.VITE_API_URL + '/users/' + email,
+            {
+              headers: {
+                'Content-Type': 'application/json',
+                Authorization: cookies.get('api_token'),
+              },
             },
-          });
-          setUserData(response.data);
+          );
+          setUserData(mapResponseToUser(response.data));
         } catch (e) {}
       })();
     }
@@ -47,13 +50,16 @@ export default function UserPage() {
         </div>
       </div>
       <div className="flex w-full px-2 md:px-64">
-        <div className="flex w-1/4 flex-col items-center bg-box-primary">
-          <img
-            src={user?.picture}
-            alt="avatar"
-            className="mx-auto mb-4 mt-8 h-32 w-32 rounded-full"
-          />
-          <h1 className="text-2xl text-text-primary">{user?.name}</h1>
+        <div className="flex w-full flex-col gap-4 md:w-1/4">
+          <div className="flex flex-col items-center rounded-sm bg-box-primary py-4">
+            <img src={user?.picture} alt="avatar" className="mx-auto mb-4 h-32 w-32 rounded-full" />
+            <h1 className="text-2xl text-text-primary">{user?.name}</h1>
+          </div>
+          <div className="flex flex-col items-center rounded-sm bg-box-primary py-4">
+            <h2 className="text-xl text-text-primary">
+              Último Login: {userData?.lastAccessAt.toLocaleDateString()}
+            </h2>
+          </div>
         </div>
       </div>
     </MainLayout>
